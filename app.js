754e8512c6e0204f2f9520cd2e1880f3a2a40e20
Idris Zaki <idriszaki0618@gmail.com>
2021-06-17 10:22:46 -0700

    data complete

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
 const express = require('express')
 const routes = require('./routes')
+const morgan = require('morgan')
+
 const app = express();
 const port = 8420;
 app.set('view engine', 'pug');
+app.use(morgan('dev'))
 
 app.use(routes)
 
+app.use((req, res, next) => {
+    const err = new Error('The requested page couldn\'t be found.');
+    err.status = 404;
+    next(err);
+  });
 
-app.listen(port, console.log(`blazing on port: ${port}`))
+app.use((err, req, res, next) => {
+    if (err.status === 404) {
+      res.status(404);
+      res.render('page-not-found', {
+        title: 'Page Not Found',
+      });
+    } else {
+      next(err);
+    }
+  });
+  
+  // Generic error handler.
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    const isProduction = process.env.NODE_ENV === 'production';
+    res.render('error', {
+      title: 'Server Error',
+      message: isProduction ? null : err.message,
+      stack: isProduction ? null : err.stack,
+    });
+  });
 
-
-
+module.exports = app
 
 
 
--- routes.js
+++ routes.js
@@ -3,8 +3,13 @@ const express = require('express');
 
 const router = express.Router();
 
+// router.get('/', (req, res) => {
+//   res.render('index', { title: 'refreshed' });
+// });
+
 router.get('/', (req, res) => {
-  res.render('index', { title: 'refreshed' });
+  // throw new Error('This is a test error!');
+  res.render('index', { title: 'Home' });
 });
 
 module.exports = router;
