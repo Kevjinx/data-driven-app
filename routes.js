4af54609a4eb714d16cd6e0a8790e92ab56b58a1
kevjinx <kevin415mei@gmail.com>
2021-06-17 15:35:08 -0700

    add express validation

--- routes.js
+++ routes.js
@@ -2,10 +2,41 @@ const express = require('express');
 const db = require('./db/models');
 const csrf  = require('csurf');
 const { response } = require('express');
+const { check, validationResult } = require('express-validator');
+
+
 
 const router = express.Router();
 
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
+const bookValidators = [
+  check('title')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Title')
+    .isLength({ max: 255 })
+    .withMessage('Title must not be more than 255 characters long'),
+  check('author')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Author')
+    .isLength({ max: 100 })
+    .withMessage('Author must not be more than 100 characters long'),
+  check('releaseDate')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Release Date')
+    .isISO8601()
+    .withMessage('Please provide a valid date for Release Date'),
+  check('pageCount')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Page Count')
+    .isInt({ min: 0 })
+    .withMessage('Please provide a valid integer for Page Count'),
+  check('publisher')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Publisher')
+    .isLength({ max: 100 })
+    .withMessage('Publisher must not be more than 100 characters long'),
+];
 
 const csrfProtection = csrf({ cookie: true })
 
@@ -51,7 +82,7 @@ router.get('/book/add', csrfProtection, (req, res, next) => {
 
 })
 
-router.post('/book/add', csrfProtection, asyncHandler(async (req, res, next) => {
+router.post('/book/add', bookValidators, csrfProtection, asyncHandler(async (req, res, next) => {
 
   const {  
     title, 
@@ -73,12 +104,17 @@ router.post('/book/add', csrfProtection, asyncHandler(async (req, res, next) => 
     await book.save();
     res.redirect('/')
   } catch (err) {
-    res.render('book-add',{
-      title: 'Add Book',
-      book, 
-      error: err,
-      csrfToken: req.csrfToken()
-    })
+    if (err.name === 'SequelizeValidationError') {
+      const errors = err.errors.map((error) => error.message)
+      res.render('book-add',{
+        title: 'Add Book',
+        book, 
+        errors,
+        csrfToken: req.csrfToken()
+      })
+    }else {
+      next(err)
+    }
   }
 }))
 
